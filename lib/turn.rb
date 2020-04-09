b06951020baf29f25a9c2d18d1473fdbd801be92
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-09 20:35:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,4 @@
-def turn(board)
-  puts "Please enter 1-9:"
-  val=input_to_index(index)
-  if valid_move?(board,val)
-    move(board, val, value)
-else
-  puts "plese enter valid number!"
-end
-end
+
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
